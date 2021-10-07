2426114a86c644b9d72c54e858bb2361ab3ae6c9
Kimberly S Brown <ksbuoregon@gmail.com>
2021-10-07 12:19:04 -0700

    refactored code - modified to make better to update later

--- module-4-assets/js/script.js
+++ module-4-assets/js/script.js
@@ -1,24 +1,34 @@
 const formEl = document.querySelector("#task-form");
 const tasksToDoEl = document.querySelector("#tasks-to-do");
 
-const createTaskHandler = function(event) {
+const taskFormHandler = function(event) {
   event.preventDefault();
   const taskNameInput = document.querySelector("input[name='task-name']").value;
   const taskTypeInput = document.querySelector("select[name='task-type']").value;
-  // create list item
-  const listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-
-  // create div to hold task info and add to list item
-  const taskInfoEl = document.createElement("div");
-  // give it a class name
-  taskInfoEl.className = "task-info";
-  // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-  listItemEl.appendChild(taskInfoEl);
-
-  // add entire list item to list
-  tasksToDoEl.appendChild(listItemEl);
+//package up data as an object
+const taskDataObj = {
+  name: taskNameInput,
+  type: taskTypeInput
 };
+//send it as an argument to createTaskEl
+createTaskEl(taskDataObj);
+}
 
-formEl.addEventListener("submit", createTaskHandler);
+
+const createTaskEl = function(taskDataObj) {
+//create list item
+const listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+//create div to hold task info and add to list item
+const taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+//add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
