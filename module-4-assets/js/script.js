eda0f115124bd8067b218087fa42f83625e96c5f
Kimberly S Brown <ksbuoregon@gmail.com>
2021-10-07 11:56:01 -0700

    add code to js to allow new tasks to entered with user input

--- module-4-assets/js/script.js
+++ module-4-assets/js/script.js
@@ -3,10 +3,21 @@ const tasksToDoEl = document.querySelector("#tasks-to-do");
 
 const createTaskHandler = function(event) {
   event.preventDefault();
-  
+  const taskNameInput = document.querySelector("input[name='task-name']").value;
+  const taskTypeInput = document.querySelector("select[name='task-type']").value;
+  // create list item
   const listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  const taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
